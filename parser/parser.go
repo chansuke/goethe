f9686d524da01411645990b3db364fcab903c7a6
chansuke <chansuke@georepublic.co.jp>
2017-07-31 17:57:30 +0900

    Fix parser

--- parser/parser.go
+++ parser/parser.go
@@ -45,7 +45,7 @@ func (p *Parser) ParseProgram() *ast.Program {
 	program := &ast.Program{}
 	program.Statements = []ast.Statement{}
 
-	for p.curToken.Type != token.EOF {
+	for !p.curTokenIs(token.EOF) {
 		stmt := p.parseStatement()
 		if stmt != nil {
 			program.Statements = append(program.Statements, stmt)
@@ -80,7 +80,9 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		return nil
 	}
 
-	for !p.curTokenIs(token.SEMICOLON) {
+	p.nextToken()
+
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
