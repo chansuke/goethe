52258f9f135bedcf02f1296e3ef9d040a70dd4b2
chansuke <chansuke@georepublic.de>
2017-11-03 02:15:19 +0900

    Enablt to parse identifier

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -107,3 +107,31 @@ func checkParserErrors(t *testing.T, p *Parser) {
 	}
 	t.FailNow()
 }
+
+func TestIdentifierExpression(t *testing.T) {
+	input := "testtest;"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has not enough statements. got =%d", len(program.Statements))
+	}
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+
+	ident, ok := stmt.Expression.(*ast.Identifier)
+	if !ok {
+		t.Fatalf("exp not *ast.Identifier. got=%T", stmt.Expression)
+	}
+	if ident.Value != "testtest" {
+		t.Errorf("ident.Value not %s. got=%s", "testtest", ident.Value)
+	}
+	if ident.TokenLiteral() != "testtest" {
+		t.Errorf("ident.TokenLiteral not %s. got=%s", "testtest", ident.TokenLiteral())
+	}
+}
