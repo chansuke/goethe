a1a1148661eced3ac8886afadc223a0dbc5f90f9
chansuke <chansuke@georepublic.de>
2017-12-21 23:43:28 +0900

    Fix error message

--- repl/repl.go
+++ repl/repl.go
@@ -37,7 +37,6 @@ func Start(in io.Reader, out io.Writer) {
 }
 
 func printParserErrors(out io.Writer, errors []string) {
-	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
 	io.WriteString(out, " parser errors:\n")
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
